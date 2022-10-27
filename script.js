7ef86a5832eb6718ad181211e4e8be5db9106854
Jonas0Dias <jonaaasdiaaas@gmail.com>
2022-10-27 10:36:34 -0300

    Manter conexão e buscar mensagens funcionando.

--- script.js
+++ script.js
@@ -1,46 +1,61 @@
 const telaentrada = window.document.querySelector('.telaentrada');
 const section = window.document.querySelector('section');
 const body = window.document.querySelector('body');
-const participantes = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants ');
 
+let nome;
+let participantes
+let status;
 function pegarNome(){
+    message = window.document.querySelector('.mensagem')
+    nome = message.value
     telaentrada.style.display='none';
     section.style.display='block';
     body.style.background="white";
-    nome = [{nome: window.document.querySelector('.mensagem').value}]
+    participantes = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: nome});
+    
+    setInterval(UsuariOnline,5000)
+    pegarMensagens();
+    setInterval(pegarMensagens,3000)
+}
+
+function UsuariOnline(resposta){
+    online = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {name: nome});
+}
+
+function pegarMensagens(){
+    
+
     // axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome );
     const mensagens = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    const participantes = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants ', nome);
+    
     
 
     
     // entrar = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants ', nome);
     // entrar.then(Verificar)
     mensagens.then(EntrarNaSala);
-    participantes.then(Verificar);
     
 
 }
 
 function EntrarNaSala(resposta){
-    console.log(resposta.data)
-    console.log()
+    
+    // console.log()
+    conteudo = document.querySelector('.conteudo')
+    conteudo.innerHTML=''
     for (let i=0; i<resposta.data.length;i++) {
         tempo = resposta.data[i].time
-        nome = resposta.data[i].from
+        usuario = resposta.data[i].from
         texto = resposta.data[i].text
-        
-        conteudo = document.querySelector('.conteudo')
         conteudo.innerHTML+=`<div>
-        <time>(${tempo})&nbsp </time><strong>${nome}&nbsp</strong><h1>${texto}</h1>
+        <time>(${tempo})&nbsp </time><strong>${usuario}&nbsp</strong><h1>${texto}</h1>
     </div>`
+        // texto.scrollIntoView();
+    
     
         
     }
     
-}
-
-function Verificar(resposta){
-    console.log(resposta.data)
+    
 }
 
