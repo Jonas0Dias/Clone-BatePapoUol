d48191b15989b130b4b5836e1b8f03628ea37584
Jonas0Dias <jonaaasdiaaas@gmail.com>
2022-10-27 02:44:09 -0300

    Mensagens recuperadas do servidor e ja adicionas no HTML.

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-telaentrada = window.document.querySelector('.telaentrada')
-section = window.document.querySelector('section')
-body = window.document.querySelector('body')
-
-
+const telaentrada = window.document.querySelector('.telaentrada');
+const section = window.document.querySelector('section');
+const body = window.document.querySelector('body');
+const participantes = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants ');
 
 function pegarNome(){
-    telaentrada.style.display='none'
-    section.style.display='block'
-    body.style.background="#DCDCDC"
-    nome = window.document.querySelector('.mensagem').value
+    telaentrada.style.display='none';
+    section.style.display='block';
+    body.style.background="white";
+    nome = [{nome: window.document.querySelector('.mensagem').value}]
+    // axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome );
+    const mensagens = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    const participantes = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants ', nome);
+    
+
+    
+    // entrar = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants ', nome);
+    // entrar.then(Verificar)
+    mensagens.then(EntrarNaSala);
+    participantes.then(Verificar);
+    
+
 }
+
+function EntrarNaSala(resposta){
+    console.log(resposta.data)
+    console.log()
+    for (let i=0; i<resposta.data.length;i++) {
+        tempo = resposta.data[i].time
+        nome = resposta.data[i].from
+        texto = resposta.data[i].text
+        
+        conteudo = document.querySelector('.conteudo')
+        conteudo.innerHTML+=`<div>
+        <time>(${tempo})&nbsp </time><strong>${nome}&nbsp</strong><h1>${texto}</h1>
+    </div>`
+    
+        
+    }
+    
+}
+
+function Verificar(resposta){
+    console.log(resposta.data)
+}
+
